f0e519fcc6734fd778ac8af27ab7874f83f40a63
Jesse Hoare Evans <jesse.hoareevans@gmail.com>
2017-05-12 10:53:11 -0700

    make two passing validations, both store and brand need to have been given a name

--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -1,5 +1,8 @@
-equire('spec_helper')
+require('spec_helper')
 
 describe(Brand) do
-  
+  it("validates that a brand name has been provided") do
+    brand = Brand.new({:name => ""})
+    expect(brand.save()).to(eq(false))
+  end
 end
--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -1,5 +1,8 @@
-equire('spec_helper')
+require('spec_helper')
 
 describe(Store) do
-  
+  it("validates that a store name has been provided") do
+    store = Store.new({:name => ""})
+    expect(store.save()).to(eq(false))
+  end
 end
