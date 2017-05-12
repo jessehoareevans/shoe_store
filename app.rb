921613d2a7f77a2a65c6f57f2587a14319a4744b
Jesse Hoare Evans <jesse.hoareevans@gmail.com>
2017-05-12 09:56:39 -0700

    basic routing

--- app.rb
+++ app.rb
@@ -7,3 +7,56 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get('/') do
   erb(:index)
 end
+
+get("/brands") do
+  @brands = Store.all()
+  erb(:brands)
+end
+
+get("/stores") do
+  @stores = Store.all()
+  erb(:stores)
+end
+
+post("/brands") do
+  name = params.fetch("brand_name")
+  Store.create({:name => name})
+  redirect("/brands")
+end
+
+post("/stores") do
+  name = params.fetch("brand_name")
+  Brand.create({:name => name})
+  redirect("/stores")
+end
+
+get("/brands/:id") do
+  @brand = Store.find(params.fetch("id").to_i())
+  if @brand.store_id
+    @store = Project.find(@brand.store_id)
+  else
+    @store = nil
+  end
+  @stores = Project.all
+  erb(:brand)
+end
+
+get("/stores/:id") do
+  @store = Project.find(params.fetch("id").to_i())
+  @brands = Store.all
+  erb(:store)
+end
+
+patch("/brands/:id") do
+  store_id = params.fetch("store_id").to_i()
+  @brand = Store.find(params.fetch("id").to_i())
+  @brand.update({:store_id => store_id})
+  redirect back
+end
+
+patch("/stores/:id") do
+  brand = Store.find(params.fetch("brand_id").to_i())
+  @store = Project.find(params.fetch("id").to_i())
+  @store.brands.push(brand)
+  redirect back
+end
