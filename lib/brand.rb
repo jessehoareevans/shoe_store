06c72db9f96398214b901298c1b0b196fe8976e4
Jesse Hoare Evans <jesse.hoareevans@gmail.com>
2017-05-20 16:07:01 -0700

    add uniqueness validation for stores and brands

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,6 +1,7 @@
 class Brand < ActiveRecord::Base
   has_and_belongs_to_many(:stores)
   validates(:name, {:presence => true, :length => { :maximum => 100}})
+  validates(:name, uniqueness: { case_sensitive: false })
   before_save(:capitalize_name)
 
   private
